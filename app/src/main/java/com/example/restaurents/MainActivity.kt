650f6814ce3555504f0d904ae987ad616d818c57
homepirate <el2409@yandex.ru>
2024-09-29 13:02:16 +0300

    add main act

---
+++ app/src/main/java/com/example/restaurents/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.restaurents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+//        if (savedInstanceState == null) {
+//            showFragment(OnboardFragment::class.java)
+//        }
+    }
+
+    public fun showFragment(fragmentClass: Class<out Fragment>) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentManager.fragments.forEach {
+            fragmentTransaction.hide(it)
+        }
+
+        val tag = fragmentClass.simpleName
+        var fragment = fragmentManager.findFragmentByTag(tag)
+        if (fragment == null) {
+            fragment = fragmentClass.newInstance()
+            fragmentTransaction.add(R.id.fragment_container, fragment, tag)
+        } else {
+            fragmentTransaction.show(fragment)
+        }
+
+        fragmentTransaction.commitNow()
+    }
+}
