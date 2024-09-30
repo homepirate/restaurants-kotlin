60c73afd6146d7d0b7d492c307ffc6bbebde9764
homepirate <el2409@yandex.ru>
2024-09-30 16:31:58 +0300

    add recommend

---
+++ app/src/main/java/com/example/restaurents/RestaurantAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.restaurents
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RestaurantAdapter(private val restaurants: List<Restaurant>) : RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder>() {
+
+
+    class RestaurantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val titleTextView: TextView = view.findViewById(R.id.titleTextView)
+        private val typeTextView: TextView = view.findViewById(R.id.typeTextView)
+        private val priceCategoryTextView: TextView = view.findViewById(R.id.priceCategoryTextView)
+
+        fun bind(restaurant: Restaurant) {
+            titleTextView.text = restaurant.name
+            typeTextView.text = restaurant.type
+            priceCategoryTextView.text = restaurant.priceCategory
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_restaurant, parent, false)
+
+        return RestaurantViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        holder.bind(restaurants[position])
+    }
+
+    override fun getItemCount(): Int {
+        return restaurants.size
+    }
+}
