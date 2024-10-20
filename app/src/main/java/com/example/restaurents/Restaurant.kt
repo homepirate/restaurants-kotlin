2fa0e3ba7a736f3c2df3f8cca98f9f462a145e15
homepirate <el2409@yandex.ru>
2024-10-20 19:22:12 +0300

    add rest list

--- app/src/main/java/com/example/restaurents/Restaurant.kt
+++ app/src/main/java/com/example/restaurents/Restaurant.kt
@@ -1,3 +1,3 @@
 package com.example.restaurents
 
-data class Restaurant(val name: String, val type: String, val priceCategory: String)
+data class Restaurant(val name: String, val type: String, val priceCategory: String, val address: String)
--- app/src/main/java/com/example/restaurents/SearchFragment.kt
+++ app/src/main/java/com/example/restaurents/SearchFragment.kt
@@ -1,13 +1,19 @@
 package com.example.restaurents
 
+import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.restaurants.R
 import com.example.restaurants.databinding.FragmentSearchBinding
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import kotlin.random.Random
 
 
 class SearchFragment : Fragment() {
@@ -29,6 +35,19 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView = binding.root.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // Загружаем данные из CSV
+        val restaurants = loadRestaurantsFromCSV(requireContext())
+
+        val adapter = RestaurantAdapter(restaurants)
+        recyclerView.adapter = adapter
+
+        // Заменяет фрагмент поиска
+        childFragmentManager.beginTransaction()
+            .replace(binding.searchFieldContainer.id, SearchFieldFragment())
+            .commitNow()
 
         childFragmentManager.beginTransaction()
             .replace(binding.searchFieldContainer.id, SearchFieldFragment()) // Ensure this matches your layout ID
@@ -39,5 +58,36 @@ class SearchFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null // Clear binding reference to prevent memory leaks
+    }
+
+
+
+    fun loadRestaurantsFromCSV(context: Context): List<Restaurant> {
+        val restaurants = mutableListOf<Restaurant>()
+
+        // Получаем доступ к ресурсу
+        val inputStream = context.resources.openRawResource(R.raw.rests) // Замените на имя вашего файла без расширения
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val priceCategories = arrayOf("$", "$$", "$$$")
+
+        reader.use { br ->
+            br.forEachLine { line ->
+                val columns = line.split(";") // Предполагается, что колонки разделены запятыми
+                if (columns.size > 8) { // Убедитесь, что количество столбцов верно
+                    val type = columns[5]
+                    if (type in listOf("кафе", "бар", "ресторан")) {
+                        val restaurant = Restaurant(
+                            name = columns[1],
+                            type = type,
+                            priceCategory = priceCategories[Random.nextInt(priceCategories.size)],
+                            address = columns[8]
+                        )
+                        restaurants.add(restaurant)
+                    }
+                }
+            }
+        }
+
+        return restaurants
     }
 }
